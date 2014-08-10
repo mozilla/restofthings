8ebce6c0488b9cbe4c21a7b36d5c39b870437fae
raluca-elena <ralucaelena1985@gmail.com>
2014-08-09 22:18:37 -0700

    spi adc 3008

--- slave/spiADCServer.js
+++ slave/spiADCServer.js
@@ -1,22 +1,10 @@
-ADC = require ('adc-pi-spi');
-options = {
-  tolerance: 10,
-  pollInterval: 200,
-  channels: [0, 1, 2, 3]
-}
-adc=new ADC('/dev/spidev0.0', options);
+var Mcp3008 = require('mcp3008.js'),
+  adc = new Mcp3008(),
+  channel = 0;
 
-adc.on ('change', function(channel, value){
-  console.log('channel ', channel, 'is now', value);
-  readValues();
-});
-
-process.on('SIGTERM', function() {
-  adc.close();});
-
-function readValues() {
-  adc.read(0, function (data) {
-    console.log("value is ----------------------", data);
+function readADC() {
+  adc.read(channel, function (value) {
+    console.log("value is----", value);
   });
-}
-
+};
+setInterval(function(){readADC();},3000);
