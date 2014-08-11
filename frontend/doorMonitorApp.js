6f62d44e1a3aecccddc43e45edb9e5554060efa0
raluca-elena <ralucaelena1985@gmail.com>
2014-08-11 15:37:41 -0700

    door monitor source added

---
+++ frontend/doorMonitorApp.js
@@ -0,0 +1,39 @@
+/**
+ * setup:
+ * query tags: - motion sensor to know if somebody is at the door
+ *             - camera to visualise who's at your door
+ *             - sound to notify when somebody/something is at the door
+ */
+function app(){
+  console.log("-----in app------");
+  queryTags(['cam', 'motion', 'sound', 'openButton'], function(tags, err) {
+    console.log("query tags response is: ", tags);
+    setInterval(monitorDoor,3000);
+    var pushMe = $('<button/>',{
+        text: 'ChangeState',
+        click: function () {
+          console.log('hi');
+          writeTag("openButton", "open", function (data) {
+            console.log("received this data as feedback from openButton", data);
+          });
+        }
+        }).addClass("btn btn-success");
+    $("#myDiv").append(pushMe);
+  });
+};
+
+function monitorDoor(){
+  readTag('motion', function(data){
+    if (data == 1){
+      console.log("got something at the door");
+      readTag('camera', function(data){
+        console.log("camera gives me this data--", data);
+        readTag('sound', function(data){
+          console.log("sound gives me this data--", data);
+        })
+      })
+    }
+  });
+}
+
+init(app);
