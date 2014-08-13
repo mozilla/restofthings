dcd6ff20bc924bf1dd1896b5275b5f4520479d58
raluca-elena <ralucaelena1985@gmail.com>
2014-08-12 21:20:50 -0700

    fix async pic

--- slave/cameraServer.js
+++ slave/cameraServer.js
@@ -29,7 +29,7 @@ app.use(cors());
 app.get("/", function(req, res) {
   var spawn = require('child_process').spawn;
   var snapshot = spawn('raspistill', ["-o", "/home/pi/restofthings/frontend/cam.jpg", "-n"]);
-  snapshot.on('close', function(res){
+  snapshot.on('close', function(){
     console.log("in close for snapshot");
     res.send("cam.jpg");
   })
