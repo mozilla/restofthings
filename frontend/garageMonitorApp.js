92f04771a367d1b16dc6c0974aa0c42c0317067e
raluca-elena <ralucaelena1985@gmail.com>
2014-08-13 11:30:16 -0700

    second simple app

--- frontend/garageMonitorApp.js
+++ frontend/garageMonitorApp.js
@@ -6,7 +6,7 @@
  */
 function app(){
   console.log("-----in app------");
-  queryTags(['light', 'relay', 'buzzer'], function(tags, err) {
+  queryTags(['light', 'buzzer'], function(tags, err) {
     console.log("query tags response is: ", tags);
     setInterval(monitorGarageDoor,3000);
     var pushMe = $('<button/>',{
@@ -14,21 +14,16 @@ function app(){
       click: function () {
         console.log('hi');
         if (this.text === 'CloseDoor') {
-        writeTag("relay", "close", function (data) {
-          console.log("received this data as feedback from openButton", data);
           writeTag("buzzer", "", function(data){
-          console.log("wrote data to buzzer ", data);
+             console.log("wrote data to buzzer ", data);
           });
           this.text = 'OpenDoor';
-        });
         } else {
-          writeTag("relay", "open", function (data) {
             console.log("received this data as feedback from openButton", data);
             this.text = 'CloseDoor';
             writeTag("buzzer", "", function(data){
               console.log("wrote data to buzzer ", data);
             });
-          });
         }
       }
     }).addClass("btn btn-success");
@@ -40,12 +35,10 @@ function monitorGarageDoor(){
   console.log("-----in monitor door-----");
   readTag('light', function(data){
     console.log("data form light sensor is", data);
-    if (data < 900){
-      console.log("got something at the door");
-      readTag('relay', function(data){
-        if (data === "open")
-          alert('GARAGE OPEN!');
-      })
+    console.log("type of data is -------", typeof(data));
+    if (parseInt(data) < 900){
+      console.log("type of data is -------", typeof(parseInt(data)));
+      console.log("------data is less than 900----");
     }
   });
 }
