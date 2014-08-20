a5666d512b09bdfc5a56a45e44fba52d8182620a
raluca-elena <ralucaelena1985@gmail.com>
2014-08-19 23:45:47 -0700

    droneApp

---
+++ frontend/apps/droneApp.js
@@ -0,0 +1,24 @@
+function app(){
+    var start = $('<button/>',{
+      id:"start",
+      text: 'takeoff',
+      click: function () {
+        ROT.writeTag("drone", "start", function(data){
+          console.log("wrote data to drone");
+        });
+      }
+    }).addClass("btn btn-success");
+    $("#myDiv").append(start);
+    var stop = $('<button/>',{
+      id:"stop",
+      text: 'land',
+      click: function () {
+        ROT.writeTag("drone", "stop", function(data){
+          console.log("wrote data to drone ");
+        });
+      }
+    }).addClass("btn btn-success");
+    $("#myDiv").append(stop);
+
+};
+ROT.init(app);
