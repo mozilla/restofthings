a14d65069ff0990937b1f63b9be4d878afb1a265
raluca-elena <ralucaelena1985@gmail.com>
2014-08-19 23:47:27 -0700

    droneApp

--- frontend/apps/droneApp.js
+++ frontend/apps/droneApp.js
@@ -1,24 +1,25 @@
 function app(){
-    var start = $('<button/>',{
-      id:"start",
-      text: 'takeoff',
-      click: function () {
-        ROT.writeTag("drone", "start", function(data){
-          console.log("wrote data to drone");
-        });
-      }
-    }).addClass("btn btn-success");
-    $("#myDiv").append(start);
-    var stop = $('<button/>',{
-      id:"stop",
-      text: 'land',
-      click: function () {
-        ROT.writeTag("drone", "stop", function(data){
-          console.log("wrote data to drone ");
-        });
-      }
-    }).addClass("btn btn-success");
-    $("#myDiv").append(stop);
+  var start = $('<button/>',{
+    id:"start",
+    text: 'takeoff',
+    click: function () {
+      ROT.writeTag("drone", "start", function(data){
+        console.log("wrote data to drone");
+      });
+    }
+  }).addClass("btn btn-success");
+  $("#myDiv").append(start);
 
+  var stop = $('<button/>',{
+    id:"stop",
+    text: 'land',
+    click: function () {
+      ROT.writeTag("drone", "stop", function(data){
+        console.log("wrote data to drone ");
+      });
+    }
+  }).addClass("btn btn-success");
+  $("#myDiv").append(stop);
 };
+
 ROT.init(app);
