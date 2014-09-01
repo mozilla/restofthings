8a5e7128ab29e0b04a27fcc850aa5a22accc71c5
raluca-elena <ralucaelena1985@gmail.com>
2014-08-31 20:58:21 -0700

    testing adc

--- slave/deviceServers/evaServer.js
+++ slave/deviceServers/evaServer.js
@@ -1,3 +1,4 @@
+var fs = require('fs');
 var ev3 = require('ev3dev');
 //Create the motor on port A
 var motorA = new ev3.Motor(ev3.MotorPort.A);
@@ -16,3 +17,7 @@ ev3.button.down.on('press', function () {
 ev3.button.up.on('release', function () {
   console.log("release");
 });
+
+//open file /sys/class/msensor/sensor1/value0 -- touch sensor
+var file =fs.openSync("/sys/class/msensor/sensor1/value0", 'r');
+console.log("file is--- ", file);
