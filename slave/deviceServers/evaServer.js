484077af656340e7f4f71687ac56d6809219c8aa
raluca-elena <ralucaelena1985@gmail.com>
2014-09-01 13:41:23 -0700

    read angle motor

--- slave/deviceServers/evaServer.js
+++ slave/deviceServers/evaServer.js
@@ -52,10 +52,7 @@ function step() {
   setTimeout(step, 4500);*/
 }
 
-for (var i = 0; i < 10; i++){
-  sleep(7000, step);
-}
-
+setInterval(step, 3000);
 
 //open file /sys/class/msensor/sensor1/value0 -- touch sensor
 var file =fs.readFileSync("/sys/class/msensor/sensor0/value0");
