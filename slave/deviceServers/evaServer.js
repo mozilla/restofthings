e575ece2809486ca817640691259acb0383fafc0
raluca-elena <ralucaelena1985@gmail.com>
2014-08-31 21:39:59 -0700

    motor first test

--- slave/deviceServers/evaServer.js
+++ slave/deviceServers/evaServer.js
@@ -2,13 +2,29 @@ var fs = require('fs');
 var ev3 = require('ev3dev');
 //Create the motor on port A
 var motorA = new ev3.Motor(ev3.MotorPort.A);
-
+var head = new ev3.Motor(ev3.MotorPort.C);
 //Run the motor at 60% power for five seconds, and then hold it in place
+
+var tachoCountA = ev3.Motor.getTachoCount('A');
+console.log("tachoCount is ---- ", tachoCountA);
 motorA.startMotor({
   targetSpeed: 40,
   time: 5000,
   stopMode: 'hold'
 });
+var speedA = ev3.Motor.getSpeed('A');
+console.log("----speed A is", speedA);
+
+var tachoCountC = ev3.Motor.getTachoCount('c');
+console.log("tachoCount is ---- ", tachoCountC);
+
+head.startMotor({
+  targetSpeed: 50,
+  time: 5000,
+  stopMode: 'hold'
+});
+var speedB = ev3.Motor.targetSpeed(C);
+console.log("----speed B is", speedB);
 
 
 //open file /sys/class/msensor/sensor1/value0 -- touch sensor
